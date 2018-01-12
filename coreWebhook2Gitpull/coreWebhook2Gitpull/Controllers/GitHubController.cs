bb263eaf7d71eda9424ccddebc6d22ca14046082
QingmingZi <qingmingzi@hotmail.com>
2018-01-12 17:06:32 +0800

    first push

---
+++ coreWebhook2Gitpull/coreWebhook2Gitpull/Controllers/GitHubController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.WebHooks;
+//using Newtonsoft.Json.Linq;
+
+namespace coreWebhook2Gitpull.Controllers
+{
+    public class GitHubController : Controller
+    {
+        public IActionResult Index()
+        {
+            //执行shell命令测试
+            string cmd = "echo hello";
+            var output = cmd.Bash();
+
+            return View();
+        }
+    }
+}
