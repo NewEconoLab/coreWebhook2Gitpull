4bc39b85edb5bf2f8fb1528ed410c7da506b5e68
QingmingZi <qingmingzi@hotmail.com>
2018-01-13 22:07:12 +0800

    实现webhook通知自动git pull基本功能，实现命令可配置，实现简单日志记录。

--- coreWebhook2Gitpull/coreWebhook2Gitpull/Controllers/GitHubController.cs
+++ coreWebhook2Gitpull/coreWebhook2Gitpull/Controllers/GitHubController.cs
@@ -1,18 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 //using Microsoft.AspNetCore.WebHooks;
-//using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace coreWebhook2Gitpull.Controllers
 {
+    [Route("api/[controller]")]
     public class GitHubController : Controller
     {
         public IActionResult Index()
         {
-            //执行shell命令测试
-            string cmd = "echo hello";
-            var output = cmd.Bash();
-
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult Get(){
+            JObject J = new JObject();
+            //执行shell命令测试
+            string bashCMD = "hostname";
+            var output = bashCMD.Bash();
+            //var output2 = "git pull origin".Bash();
+
+            //JObject Json = new JObject();
+            J.Add("bashCMD", bashCMD);
+            J.Add("bashCMDout", output);
+            //J.Add("put1", output2);
+            return Json(J);
+        }
+
+        [HttpPost]
+        public JsonResult Post([FromBody] JObject J)
+        {
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection()    //将配置文件的数据加载到内存中
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())   //指定配置文件所在的目录
+                .AddJsonFile("settings.json", optional: true, reloadOnChange: true)  //指定加载的配置文件
+                .Build();    //编译成对象  
+            string bashCMD = config["bashCMD"];
+            //执行shell命令测试
+            var output = bashCMD.Bash();
+            //var output2 = "git pull origin".Bash();
+
+            //JObject Json = new JObject();
+            J.Add("bashCMD", bashCMD);
+            J.Add("bashCMDout", output);
+            //J.Add("put1", output2);
+            string resStr = JsonConvert.SerializeObject(J);
+            System.IO.File.WriteAllText(System.IO.Directory.GetCurrentDirectory() + "/log/" + System.DateTime.Now.ToString ("yyyyMMddHHmmssms") + ".log",resStr);
+
+            return Json(J);
         }
     }
 }
