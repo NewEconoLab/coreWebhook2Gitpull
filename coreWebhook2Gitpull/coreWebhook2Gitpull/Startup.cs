2ce47381c6825417f8c373672f6594c37c904565
tssqzr <1214661466@qq.com>
2020-03-02 17:30:26 +0800

    允许跨域请求

--- coreWebhook2Gitpull/coreWebhook2Gitpull/Startup.cs
+++ coreWebhook2Gitpull/coreWebhook2Gitpull/Startup.cs
@@ -29,6 +29,9 @@ namespace coreWebhook2Gitpull
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
